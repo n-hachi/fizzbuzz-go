74c1a8348161d27156fd1a82f8d08694b05c1af3
Nozomu Hatta <nozomuhatta@gmail.com>
2021-12-04 02:40:59 +0900

    Fix Fizzbuzz func

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -5,12 +5,12 @@ import "fmt"
 // Fizzbuzz takes one postive value and return string "Fizz", "Buzz", "FizzBuzz" or number.
 func Fizzbuzz(n uint64) string {
 	switch {
+	case n%15 == 0:
+		return "FizzBuzz"
 	case n%3 == 0:
 		return "Fizz"
 	case n%5 == 0:
 		return "Buzz"
-	case n%15 == 0:
-		return "FizzBuzz"
 	default:
 		return fmt.Sprintf("%d", n)
 	}
