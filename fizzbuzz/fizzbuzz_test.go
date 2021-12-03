3a2ae31088d7bce81e76c433fda8e5c0b6377b25
Nozomu Hatta <nozomuhatta@gmail.com>
2021-12-04 01:05:39 +0900

    Add minimum test content

--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -3,4 +3,10 @@ package fizzbuzz
 import "testing"
 
 func TestFizzbuzz(t *testing.T) {
+	// Check number string
+	n1 := uint64(1)
+	s1 := "1"
+	if s1 != Fizzbuzz(n1) {
+		t.Errorf("Fizzbuzz(1) = %v but expected %v\n", Fizzbuzz(n1), s1)
+	}
 }
